3408791f5a0c70ec41b40b9d0edd954571bc99b4
DeviNur <41379185+DeviNur@users.noreply.github.com>
2019-05-19 20:56:01 +0700

    Add files via upload

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id7899377_sertifikasi","santi123","id7899377_santi") or die ("could not connect database");
+?>
